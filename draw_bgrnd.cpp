1e670c9728801246c8b74348e83da9762be5ce08
MariaMazaieva <mazaevamasha@gmail.com>
2025-05-29 19:25:02 +0200

    Deleted unused files

--- draw_bgrnd.cpp
+++
@@ -1,8 +0,0 @@
-#define WIDTH 480
-#define HEIGHT 320
-
-void draw_background(unsigned short* fb, unsigned short background_color) {
-    for (int i = 0; i < WIDTH * HEIGHT; i++) {
-        fb[i] = background_color;
-    }
-}
--- draw_bgrnd.h
+++
@@ -1,1 +0,0 @@
-void draw_background(unsigned short* fb, unsigned short background_color);
